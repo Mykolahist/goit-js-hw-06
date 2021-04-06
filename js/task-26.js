f112421171bcbc09d0972027d614769edde9173d
Mykolahist <71245049+Mykolahist@users.noreply.github.com>
2021-04-06 23:04:31 +0300

    task-30

---
+++ js/task-26.js
@@ -0,0 +1,20 @@
+const firstArray = [26, 94, 36, 18];
+const secondArray = [17, 61, 23];
+const thirdArray = [17, 26, 94, 61, 36, 23, 18];
+// Пиши код ниже этой строки
+
+const eachElementInFirstIsEven = firstArray.every(value => value % 2 === 0);
+const eachElementInFirstIsOdd = firstArray.every(value => value % 2 !== 0);
+
+const eachElementInSecondIsEven = secondArray.every(value => value % 2 === 0);
+const eachElementInSecondIsOdd = secondArray.every(value => value % 2 !== 0);
+
+const eachElementInThirdIsEven = thirdArray.every(value => value % 2 === 0);
+const eachElementInThirdIsOdd = thirdArray.every(value => value % 2 !== 0);
+
+console.log(eachElementInFirstIsEven);
+console.log(eachElementInFirstIsOdd);
+console.log(eachElementInSecondIsEven);
+console.log(eachElementInSecondIsOdd);
+console.log(eachElementInThirdIsEven);
+console.log(eachElementInThirdIsOdd);
